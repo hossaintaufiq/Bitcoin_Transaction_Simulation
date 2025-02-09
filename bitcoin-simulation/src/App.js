36dde7e744cd98ff7eaceb6ee5baece14c3f55d2
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-02-09 19:55:43 +0600

    creating the home page and header

--- bitcoin-simulation/src/App.js
+++ bitcoin-simulation/src/App.js
@@ -1,11 +1,17 @@
 // import logo from "./logo.svg";
+import {BrowserRouter,Route} from "react-router-dom"
+import Header from "./components/Header"
 import "./App.css";
+import Homepage from "./Pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      hello world 
+    <BrowserRouter>
+    <div>
+      <Header/> 
+      <Route path ='/' Component={Homepage}/>
     </div>
+    </BrowserRouter>
   );
 }
 
